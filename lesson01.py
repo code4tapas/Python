982b69bce2ba56a66162299b34fff96fb774eb01
code4tapas <tapas.chowdhury@nice.com>
2024-03-03 21:06:50 +0530

    practice lesson added

---
+++ lesson01.py
@@ -0,0 +1,32 @@
+# this is practice session 01
+
+# Q1. Take two number ind print sum
+# number1 = int (input("enter first number: "))
+# number2 = int(input("enter second number: "))
+# print("Sum of number one and number2 is" , number1 + number2)
+
+# Q2. Input the length of the hand of a squre and print area
+# Length =  int (input("enter length length: "))
+# print ("Area of the squre is -", Length * 4)
+
+# Q3. WAP to input 2 floating point numbers & print their average.
+
+# fp01 = float(input("Number 01 : "))
+# fp02 = float(input("Number 02 : "))
+# print("Average is:", (fp01 + fp02) / 2)
+
+# Q3. WAP to input 2 int numbers, a and b.
+#     Print True if a is greater than or equal to b. If not print False.
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+
+if a > b :
+     print ("a is > b")
+elif a == b:
+    print ("a is = b")
+else:
+    print ("b > a") 
+   
+
+
